9fbaa87b7e5a345a09006d1550f0c968cce8f054
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2016-06-02 13:46:14 +0200

    Implement a rudimentary brainbow

--- src/Main.elm
+++ src/Main.elm
@@ -1,4 +1,137 @@
-import Html exposing (text)
+import Html exposing (..)
+import Html.App exposing (beginnerProgram)
+import Html.Events exposing (onClick)
+
 
 main =
-  text "Hello, World!"
+  beginnerProgram {
+    model = init 6
+  , view = view
+  , update = update
+  }
+
+
+-- MODEL
+
+
+type alias Model =
+  { permutation: List Int }
+
+
+init : Int -> Model
+init n =
+  {
+    permutation = range n
+  }
+
+
+range : Int -> List Int
+range n =
+  sequence 0 n
+
+
+sequence : Int -> Int -> List Int
+sequence a b =
+  if a >= b then
+     []
+  else
+    [a] ++ sequence (a + 1) b
+
+
+rotate : List a -> List a
+rotate permutation =
+  case permutation of
+    [] ->
+      []
+
+    x :: xs ->
+      xs ++ [x]
+
+
+swapFirst : List a -> List a
+swapFirst permutation =
+  let
+    parts =
+      equalize ([], permutation)
+
+    first =
+      fst parts
+
+    second =
+      snd parts
+  in
+    (reverse first) ++ second
+
+
+reverse : List a -> List a
+reverse permutation =
+  case permutation of
+    [] ->
+      []
+
+    x :: xs ->
+      (reverse xs) ++ [x]
+
+
+split : List a -> (List a, List a)
+split permutation =
+  equalize ([], permutation)
+
+
+equalize : (List a, List a) -> (List a, List a)
+equalize (xs, ys) =
+  if (List.length xs >= List.length ys) then
+    (xs, ys)
+  else
+    case ys of
+      [] ->
+        (xs, ys)
+
+      z :: zs ->
+        equalize (xs ++ [z], zs)
+
+
+-- UPDATE
+
+
+type Message =
+  Rotate
+  | Swap
+
+
+update : Message -> Model -> Model
+update msg model =
+  case msg of
+    Rotate ->
+      { model | permutation = rotate model.permutation }
+
+    Swap ->
+      { model | permutation = swapFirst model.permutation }
+
+
+-- VIEW
+
+
+view : Model -> Html Message
+view model =
+  let
+    rotate =
+      button [ onClick Rotate ] [ text "r" ]
+
+    swap =
+      button [ onClick Swap ] [ text "s" ]
+
+    permutation =
+      viewPermutation model.permutation
+  in
+    div []
+        ([ rotate, swap ] ++ permutation)
+
+
+viewPermutation : List a -> List (Html Message)
+viewPermutation permutation =
+  List.map viewElement permutation
+
+viewElement : a -> Html Message
+viewElement n =
+  span [] [ text (toString n) ]
